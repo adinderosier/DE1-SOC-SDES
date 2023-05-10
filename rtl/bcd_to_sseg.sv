2a63e06aaf90a73188ec9a70275f013ade7a149a
cashtache <55feilm@gmail.com>
2023-05-10 13:03:55 -0700

    State machine initial implementation

--- rtl/bcd_to_sseg.sv
+++ rtl/bcd_to_sseg.sv
@@ -9,26 +9,25 @@ module bcd_to_sseg (
 
     // BCD to seven-segment display conversion using a case statement
     always_comb begin
-         case (i_bcd)
-              4'b0000: o_sseg = 7'b1000000; // 0
-              4'b0001: o_sseg = 7'b1111001; // 1
-              4'b0010: o_sseg = 7'b0100100; // 2
-              4'b0011: o_sseg = 7'b0110000; // 3
-              4'b0100: o_sseg = 7'b0011001; // 4
-              4'b0101: o_sseg = 7'b0010010; // 5
-              4'b0110: o_sseg = 7'b0000010; // 6
-              4'b0111: o_sseg = 7'b1111000; // 7
-              4'b1000: o_sseg = 7'b0000000; // 8
-              4'b1001: o_sseg = 7'b0010000; // 9
-              4'b1010: o_sseg = 7'b0001000; // A
-              4'b1011: o_sseg = 7'b0000011; // B
-              4'b1100: o_sseg = 7'b1000110; // C
-              4'b1101: o_sseg = 7'b0100001; // D
-              4'b1110: o_sseg = 7'b0000110; // E
-              4'b1111: o_sseg = 7'b0001110; // F
-              default: o_sseg = 7'b1111111; // Invalid BCD input, turn off all segments
-         endcase
+        case (i_bcd)
+            4'b0000: o_sseg = 7'b1000000; // 0
+            4'b0001: o_sseg = 7'b1111001; // 1
+            4'b0010: o_sseg = 7'b0100100; // 2
+            4'b0011: o_sseg = 7'b0110000; // 3
+            4'b0100: o_sseg = 7'b0011001; // 4
+            4'b0101: o_sseg = 7'b0010010; // 5
+            4'b0110: o_sseg = 7'b0000010; // 6
+            4'b0111: o_sseg = 7'b1111000; // 7
+            4'b1000: o_sseg = 7'b0000000; // 8
+            4'b1001: o_sseg = 7'b0010000; // 9
+            4'b1010: o_sseg = 7'b0001000; // A
+            4'b1011: o_sseg = 7'b0000011; // B
+            4'b1100: o_sseg = 7'b1000110; // C
+            4'b1101: o_sseg = 7'b0100001; // D
+            4'b1110: o_sseg = 7'b0000110; // E
+            4'b1111: o_sseg = 7'b0001110; // F
+            default: o_sseg = 7'b1111111; // Invalid BCD input, turn off all segments
+        endcase
     end
 
 endmodule
-
--- rtl/permutation_p10.sv
+++ rtl/permutation_p10.sv
@@ -9,7 +9,7 @@ module permutation_p10 (
 
     // Assign output signal as a permutation of input signal bits
     assign o_signal = {i_signal[7], i_signal[5], i_signal[8], i_signal[3],
-                      i_signal[6], i_signal[0], i_signal[9], i_signal[1],
-                      i_signal[2], i_signal[4]};
+							i_signal[6], i_signal[0], i_signal[9], i_signal[1],
+							i_signal[2], i_signal[4]};
 
 endmodule
